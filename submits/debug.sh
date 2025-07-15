8ffa81230bad296c2b627d905110b6f4e794313c
florianbarkmann <flobarkmann@googlemail.com>
2025-07-15 11:07:14 +0200

    debug submit script.

---
+++ submits/debug.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=snakemake
+#SBATCH --time=12:00:00
+#SBATCH --partition=compute
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+
+
+srun snakemake  --use-singularity --singularity-args "--nv" --executor slurm  --configfile configs/integration/debug2.yaml -s integration.smk --keep-going
